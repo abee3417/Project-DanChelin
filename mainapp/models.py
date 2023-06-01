f0ae2c81f07c3498ab093490c6a7912ac01e6e4e
Yunseong Yang <abee3417@naver.com>
2023-06-01 14:42:41 +0900

    Update Danchelin to ver 4

--- mainapp/models.py
+++ mainapp/models.py
@@ -4,16 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     category = models.IntegerField() # 1~11까지로 한식부터 카페까지 구분하기 위한 필드
-    subject = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
-    callnum = models.CharField(max_length=200)
-    timeinfo = models.CharField(max_length=200)
-    urlinfo = models.CharField(max_length=200)
-    menu = models.TextField()
-    review = models.TextField()
-    image = models.ImageField(blank=True, upload_to='media/')
-    # 별점 필드 추가하여 넣을 것.
-    voter = models.ManyToManyField(User)  # 추천인 추가
+    subject = models.CharField(max_length=200) # 가게이름
+    location = models.CharField(max_length=200) # 주소
+    callnum = models.CharField(max_length=200) # 전화번호
+    timeinfo = models.CharField(max_length=200) # 영업시간
+    urlinfo = models.CharField(max_length=200) # URL
+    menu = models.TextField() # 메뉴
+    review = models.TextField() # 총평
+    image = models.ImageField(blank=True, upload_to='media/') # 썸네일사진
+    summary = models.CharField(max_length=200) # 썸네일텍스트
+    voter = models.ManyToManyField(User)  # 추천인
 
     def __str__(self):
         return self.subject
--- mainapp/views.py
+++ mainapp/views.py
@@ -15,6 +15,12 @@ def detail(request, post_id):
     return render(request, 'mainapp/detail.html', context) # question_detail.html에 있는 question에 context를 전달
 
 @login_required(login_url='user:login')
+def vote(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.voter.add(request.user) # 추천인 속성에 유저를 추가
+    return redirect('main:detail', post_id=post.id)
+
+@login_required(login_url='user:login')
 def comment_create(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if request.method == "POST":
@@ -67,3 +73,11 @@ def list2(request):
     post_list = Post.objects.filter(category=2) # Post 중 카테고리 2 (일식)만 반환
     context = {'post_list': post_list}
     return render(request, 'mainapp/list2.html', context)
+
+def list3(request):
+    post_list = Post.objects.filter(category=3) # Post 중 카테고리 3 (중식)만 반환
+    context = {'post_list': post_list}
+    return render(request, 'mainapp/list3.html', context)
+
+def list4(request):
+    return render(request, 'mainapp/list4.html')
